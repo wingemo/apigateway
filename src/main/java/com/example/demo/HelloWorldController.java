b734e520a7ec8de46b75468424081491bb90cc75
Philip Wingemo <philip.wingemo@outlook.com>
2024-01-13 19:42:40 +0100

    Broken validation

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -5,13 +5,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.FieldError;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.Valid;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/newsflash")
